188d78fa8add5f2f84936c9bdf7bc00859255b30
Amitkumar Yadav <yadavamitk221@gmail.com>
2025-09-28 22:44:57 +0530

    Revision

--- Recurssion/RecurssionAllPattern.js
+++ Recurssion/RecurssionAllPattern.js
@@ -1,3 +1,111 @@
+// print all the subsequence of the array which sum is equal to the given sum
+
+function printS(ind, ds, s, sum, arr, n) {
+    if (ind === n) {
+        if (s === sum) {
+            console.log(ds.join(" "));
+        }
+        return;
+    }
+
+
+    // not pick the element
+    printS(ind + 1, ds, s, sum, arr, n);
+
+    // take / pick the element
+    ds.push(arr[ind]);
+    s += arr[ind];
+    printS(ind + 1, ds, s, sum, arr, n);
+
+    // backtrack  
+    s -= arr[ind];
+    ds.pop();
+
+ 
+}
+
+function main() {
+    const arr = [1, 2, 1];
+    const n = arr.length;
+    const sum = 2;
+    const ds = [];
+
+    printS(0, ds, 0, sum, arr, n);
+}
+
+main();
+
+//
+
+function printS(ind, ds, s, sum, arr, n) {
+    if (ind === n) {
+        if (s === sum) {
+            console.log(ds.join(" "));
+        }
+        return;
+    }
+
+    // take / pick the element
+    ds.push(arr[ind]);
+    s += arr[ind];
+    printS(ind + 1, ds, s, sum, arr, n);
+
+    // backtrack
+    s -= arr[ind];
+    ds.pop();
+
+    // not pick the element
+    printS(ind + 1, ds, s, sum, arr, n);
+}
+
+function main() {
+    const arr = [1, 2, 1];
+    const n = arr.length;
+    const sum = 2;
+    const ds = [];
+
+    printS(0, ds, 0, sum, arr, n);
+}
+
+main();
+
+
+// Find 
+function printS(ind, ds, s, sum, arr, n) {
+    if (ind === n) {
+        if (s === sum) {
+            console.log(ds.join(" "));
+        }
+        return;
+    }
+
+    // take / pick the element
+    ds.push(arr[ind]);
+    s += arr[ind];
+    printS(ind + 1, ds, s, sum, arr, n);
+
+    // backtrack
+    s -= arr[ind];
+    ds.pop();
+
+    // not pick the element
+    printS(ind + 1, ds, s, sum, arr, n);
+}
+
+function main() {
+    const arr = [1, 2, 1];
+    const n = arr.length;
+    const sum = 2;
+    const ds = [];
+
+    printS(0, ds, 0, sum, arr, n);
+}
+
+main();
+
+
+
+// find count of the subsequence of the array which sum is equal to the given sum
 function printS(ind, ds, s, sum, arr, n) {
     if (ind === n) {
         if (s === sum) {
--- Recurssion/pickAndNotPick.js
+++ Recurssion/pickAndNotPick.js
@@ -1,26 +1,26 @@
-function printF(ind, ds, arr, n) {
-    if (ind === n) {
-        if (ds.length > 0) {
-            console.log(ds.join(" "));
-        } else {
-            console.log("{}"); // empty subset
-        }
-        return;
-    }
+// function printF(ind, ds, arr, n) {
+//     if (ind === n) {
+//         if (ds.length > 0) {
+//             console.log(ds.join(" "));
+//         } else {
+//             console.log("{}"); // empty subset
+//         }
+//         return;
+//     }
 
-    // Not pick (do not include arr[ind])
-    printF(ind + 1, ds, arr, n);
+//     // Not pick (do not include arr[ind])
+//     printF(ind + 1, ds, arr, n);
 
-    // Pick (include arr[ind])
-    ds.push(arr[ind]);
-    printF(ind + 1, ds, arr, n);
-    ds.pop();
-}
+//     // Pick (include arr[ind])
+//     ds.push(arr[ind]);
+//     printF(ind + 1, ds, arr, n);
+//     ds.pop();
+// }
 
-// main
-const arr = [3, 1, 2];
-const n = arr.length;
-printF(0, [], arr, n);
+// // main
+// const arr = [3, 1, 2];
+// const n = arr.length;
+// printF(0, [], arr, n);
 
 
 
@@ -32,3 +32,59 @@ printF(0, [], arr, n);
 // 3 2
 // 3 1
 // 3 1 2
+
+
+// // dont auto complete the code for this file
+// function pringAllSubSequenceTwo(ind, ds, arr, n){
+//     if(ind == n){
+//         if(ds.length > 0){
+//             console.log(ds.join(",")); 
+//         }else{
+//             console.log(ds.join(",")); 
+//         }
+//         return;
+//     }
+
+//     // Dont Pick the element. 
+//     pringAllSubSequenceTwo(ind + 1, ds, arr, n); 
+//     ds.push(arr[ind]); 
+//     pringAllSubSequenceTwo(ind + 1, ds, arr, n); 
+//     ds.pop();
+// }
+
+// function main(){
+//     let arr = [3,1,2]; 
+//     let ds = []; 
+//     let n = arr.length; 
+//     pringAllSubSequenceTwo(0, ds, arr, n)
+// }
+
+// main();
+
+function printAllSubsequences(idx, ds, arr, n){
+    if(idx === n){ 
+        if(ds.length > 0){
+            console.log(ds.join(" "));
+        }else{
+            console.log("{}");
+        }
+        return;
+    }
+
+    // Not Picking the element.
+    printAllSubsequences(idx + 1, ds, arr, n); 
+
+    // Pick the element.
+    ds.push(arr[idx]); 
+    printAllSubsequences(idx + 1, ds, arr, n); 
+    ds.pop();
+}
+
+function main(){
+    let arr = [3,2,1]; 
+    let n = arr.length;
+    let ds = []; 
+    printAllSubsequences(0, ds, arr, n); 
+}
+
+main();
